535b8f1b8439a39bdbf686b5b021d790d6964ea4
stefanicjuraj <jurajstefanic@outlook.com>
2024-11-27 22:32:03 +0100

    feat: add validate github url

--- docs2llms.sh
+++ docs2llms.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+validate_github_url() {
+    local url="$1"
+    if [[ "$url" =~ ^https://github.com/[^/]+/[^/]+(/tree/[^/]+/.+)?$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
