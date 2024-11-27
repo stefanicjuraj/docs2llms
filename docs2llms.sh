c67501ee6f484403819c66782e2ba52421960a54
stefanicjuraj <jurajstefanic@outlook.com>
2024-11-27 22:32:22 +0100

    feat: add parse github url

--- docs2llms.sh
+++ docs2llms.sh
@@ -8,3 +8,13 @@ validate_github_url() {
         return 1
     fi
 }
+
+parse_github_url() {
+    local url="$1"
+    owner=$(echo "$url" | awk -F '/' '{print $4}')
+    repo=$(echo "$url" | awk -F '/' '{print $5}')
+    branch=$(echo "$url" | awk -F '/' '{print $7}')
+    path=$(echo "$url" | cut -d '/' -f8-)
+    branch="${branch:-main}"
+    path="${path:-}"
+}
